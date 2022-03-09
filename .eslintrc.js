f27be259566f396bed93013100f9bf5484b9c402
taiki <101173435+Inoue-if@users.noreply.github.com>
2022-03-09 19:02:49 +0900

    Install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
